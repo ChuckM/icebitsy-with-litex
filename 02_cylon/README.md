8a5f28cb1f27438cfb16b65987c3490e804d6a02
Chuck McManis <cmcmanis@mcmanis.com>
2021-09-16 23:15:29 -0700

    Updated Cylon example (now example 02)

--- 02_cylon/README.md
+++ 02_cylon/README.md
@@ -4,25 +4,33 @@ New features, new module
 Now that we've proven the toolchain works, this example goes on to
 use some more features of LiteX.
 
-The first thing is that this example uses the 'built in' board
-definition for the 1bitsquared Icebreaker board. What that gives us is
-that all the pins are already defined so we don't have to define them
-and it uses a new construct called `Connector` which describes the three
-PMOD ports (1A, 1B, and 2).
+The first thing is that this example changes is that it uses the 
+'built in' platform definition for the 1Bitsquared Icebitsy board.
+That means less typing in our code and, in theory, it would track
+versions of the Icebitsy as they were released. It also means that
+all the pins are already defined so we don't have to define them. It
+also uses a new construct class `Connector` to describe the three
+PMOD ports (1, 2, and 3). 
 
 The second thing this example does is to programmatically define the signals
 of a PMOD, in this case the 
 [Digilent 8 LED PMOD](https://store.digilentinc.com/pmod-8ld-eight-high-brightness-leds/).
-The code in `led8_mod` takes a single argument, the connector that the PMOD
-is plugged into. Since we are using two we use this function twice, once for
-the PMOD connected to PMOD1A and once for the PMOD connected to PMOD1B.
 
-That is followed by a call to `platform.add_extension()` which adds those
+The code in `gen_led8` takes two arguments, the connector that the PMOD
+is plugged into and the instance number. Since we are using two we use
+this function twice, once for the PMOD connected to PMOD1 and once for
+the PMOD connected to PMOD2.
+
+That is followed by a call to `icebitsy.add_extension()` which adds those
 PMOD signal resources to our design.
 
-And finally our simple design module, called `Chaser` expects that these LED8
-PMODs are present, and it finds them using `platform.request("led8")` which
+And finally our simple design module, called `Cylon` expects that these LED8
+PMODs are present, and it finds them using `icebitsy.request("led8")` which
 it does twice, to get access to the signal definitions for both PMODs.
+
+Another feature of this example is that it creates a bus named, `all_leds`,
+using the `Cat(...)` class, that defines a bus of all 16 leds as a single
+16 wire bus.
 
 The module defines `display` which holds the state of 16 LEDs in it, and
 because it is assigned in the `sync` block it will become a register. There
@@ -30,18 +38,14 @@ is also a 1 bit flip flop assigned to the value `direction` to indicate if
 the chaser is moving left or right.
 
 The same count down timer/divider is used to convert the clock in signal to
-a clock at the frequency that the LED will change state. I find 10Hz provides
-a reasonable speed.
+a clock at the frequency that the LED will change state.
 
 In the combinatorial section, the value in the `display` register is routed
-to the LED8 PMODs. This is done with an assignment much like the `Blinker`
-code in example 1 did but with more LEDs. I've left in the code the three
-different ways I tried to make this work. Two of them do what I wanted and
-one does not.
+to the LED8 PMODs. This is done with an assignment much like the `Blink`
+code in example `01_blink`  did but with more LEDs.
 
-The final working solution (which I like because the intent is clear without
-being too wordy), was to use the `Cat()` operation to create a new 16 wide 
-Signal called `all_leds` which aliases the two PMOD's individual signals
-to a single bus. The combinatorial code then does an assignment to this
-bus from the state of the `display` register and it appears on the LEDs.
+When writing this code I learned some importatant stuff about 
+using `Signal(...)` and the names of wires. I've left in the code the three
+different ways I tried to make this work. Two of them actually do what I
+wanted and one does not.
 
--- 02_cylon/cylon.py
+++ 02_cylon/cylon.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
 #
-# Another slightly less litex example, to get a feel for the flow
-# from using litex.
-#
 # Written by Chuck McManis August, 2021
 # Not a lot of useful stuff here feel free to use it how you would like.
 #
@@ -13,67 +10,115 @@ from migen import *
 # base class is the Platform()
 from litex.build.generic_platform import *
 
-# It is a known board so import that instead
+#
+# Okay, so unlike in 01_Blink, this version just grabs the predefined
+# platform from litex_boards.platform
+#
 from litex_boards.platforms.icebreaker_bitsy import Platform
 
 #
-# First off create a Platform instance from the imported icebreaker 
+# Create an instance of an icebitsy from it. This has the various LEDs
+# and connectors pre-defined.
 #
-platform = Platform()
+icebitsy = Platform()
 
 #
-# Two steps in the design process here, one is some code to describe
-# how the Digilent PMODs are connected, the other is the actual
-# LED Chaser code.
+# Using python to automate some typing.
 #
-# Step 1: Define the Digilent PMOD
+# This function loops through 0 to 7 and defines Subsignals of the form
+# "led<n>", "PMODx:<n>".
 #
-# This is fairly simple code to express a Digilent LED8 PMOD connected
-# to one of the icebreaker's PMOD ports. (PMOD1A, PMOD1B, PMOD2)
+# Two interesting things going on here, Subsignals and Connections.
 #
-# Once added to the platform as an extension, you can access the
-# individual LEDs as <name>.led0 thru <name>.led7.
+# Subsignal(...) gives a name to one or more of the signals that are part of a
+# Signal(...) definition. If you look in the platform definition for the
+# icebitsy you will see there is a definition of 'serial' with two sub-signals
+# one called 'rx' and one called 'tx'. This creates a new signal called 'led8'
+# which has 8 sub-signals led0 through led7.
 #
-def led8_pmod(pmod_port):
-	return [
-		("led8", 0,
-			Subsignal("led0", Pins(f"{pmod_port}:0")),
-			Subsignal("led1", Pins(f"{pmod_port}:1")),
-			Subsignal("led2", Pins(f"{pmod_port}:2")),
-			Subsignal("led3", Pins(f"{pmod_port}:3")),
-			Subsignal("led4", Pins(f"{pmod_port}:4")),
-			Subsignal("led5", Pins(f"{pmod_port}:5")),
-			Subsignal("led6", Pins(f"{pmod_port}:6")),
-			Subsignal("led7", Pins(f"{pmod_port}:7")),
-		)
-	]
+# Connections(...) are I/O pins that go off board or to a connector. In this
+# case there are three PMOD connectors, each has eight I/Os. In the platform
+# definition file they are assigned to pins on the FPGA and named one of PMOD1
+# PMOD2, or PMOD3. Unlike signals they are simply a text map from a connector
+# name : pin number (0 - n) to the string to give to the Pin(...) class that
+# connects that to the FPGA proper. So PMOD3:0 is pin 0, of connector PMOD3.
+#
+# The net effect of this code is to generate an 8 bit wide signal with each
+# line of that signal having its own subname. They are then used in module
+# code with something like "<SignalName>.led0" to talk to the first LED.
+#
+def gen_led8(pmod_port, num):
+	led8 = ["led8", num]
+	for i in range(8):
+		led8.append(
+			Subsignal(f"led{i}", Pins(f"{pmod_port}:{i}"),
+								 IOStandard("LVCMOS33")))
+	return tuple(led8)
 
 #
-# Two LED8 Pmods are attached, one to PMOD1A and one to PMOD1B
+# Two LED8 Pmods are attached, one to PMOD1 and one to PMOD2.
+# The gen_led8 code creates a tuple that defines an "extension." Module
+# code can then request that extension from the platform later and that
+# is what we do in this code. We add a different unit number to them so
+# that we can pick which one we want to wire up in our module code.
 #
-platform.add_extension(led8_pmod("PMOD1"));
-platform.add_extension(led8_pmod("PMOD2"));
+icebitsy.add_extension([gen_led8("PMOD1", 0)])
+icebitsy.add_extension([gen_led8("PMOD2", 1)])
 
 #
-# Step 2: Define a module that is a "Chaser" (basically a set of leds
-# that blink in sequence.  Like example 1 the counter/divider set up
-# is used to provide a slower clock source. 
-# 
-# This code uses the PMODs that were added as extensions above.
+# At this point we have an augmented platform with extensions describing
+# the connection to two LED8 PMODs. At this point then we define a top
+# level module like we did in Blink to send some data to them.
 #
-class Chaser(Module):
-	def __init__(self, blink_freq, sys_clk_freq):
-		leds = platform.request("led8")
-		more_leds = platform.request("led8")
-		# note: they have a reset value of '1'
+# If you don't understand why it's called 'Cylon' look up Battlestar
+# Galactica on the Internet :-). I suppose I could also name it Night Rider
+# but that is more typing.
+#
+class Cylon(Module):
+	"""
+		A module that has an LED bouncing back and forth on two LED8
+		PMODs connected to PMOD port 1 and PMOD port 2
+	"""
+
+	def __init__(self, blink_freq):
+		#
+		# As with the Blink example, we "request" the signals associated
+		# with the name "led8" because we're going to be talking to them.
+		#
+		leds = icebitsy.request("led8", 0)
+		more_leds = icebitsy.request("led8", 1)
+		#
+		# Now we define a 'display' register which will hold the state
+		# of 16 LEDs and we add the parameter 'reset=1' which means that
+		# the reset state will set the value to 0b0000000000000001
 		display = Signal(16, reset=1)
-		direction = Signal(1, reset=1)
 
-#
-# This uses the 'Cat()' (concatenate) construct, to create a new signal
-# that is an amalgam of all the LEDs. This makes it easier to set them
-# all at once in the comb section below.
-#
+		#
+		# Direction remembers if we are going left or right.
+		#
+		direction = Signal(1)
+
+		#
+		# Same counter/ticks like we used in Blink, look at the comments
+		# in that code for why 24 is enough bits of counter or how we
+		# compute ticks.
+		counter = Signal(24)
+		ticks = int(500e6 / (blink_freq * icebitsy.default_clk_period)) - 1
+
+
+		#
+		# And this is a contrived example (in a planned example they would have
+		# been defined this way) but it demonstrates an FHDL function i
+		# 'Cat(...)' which is short for concatenate.
+		#
+		# This code is essentially punning (symlinking?) the 16 leds in the
+		# two LED8 PMODs into a single bus called "all_leds" which has a 'shape'
+		# of 16. What that means is we can assign a 16 bit value to this bus
+		# and it will change all of the LEDs at once. (We don't need to turn
+		# each one on individually). Now this example is just adding them in
+		# order, but the nice thing here is that you could combine a bunch of
+		# signals into a single bus for easier assignment or reading.
+		#
 		all_leds = Cat(
 				leds.led0, leds.led1, leds.led2, leds.led3,
 				leds.led4, leds.led5, leds.led6, leds.led7,
@@ -81,11 +126,30 @@ class Chaser(Module):
 				more_leds.led4, more_leds.led5, more_leds.led6, more_leds.led7,
 		)
 
-		# this makes a 32 bit counter
-		counter = Signal(32)
+		#
+		# Now for the synchronous part.
+		#
+		# When enough ticks have gone by (the If() statement) the code does
+		# the following:
+		#
+		#	if the direction is LEFT then
+		#		if the display is 0b1000...0 then
+		#			set the direction to RIGHT
+		#		else
+		#			rotate the display one bit left
+		#	else
+		#		if the display is 0b000...1 then
+		#			set the direction to LEFT
+		#		else
+		#			rotate the display one bit right
+		#
+		# Many will immediately recognize that this is the algorithm for
+		# having a light ping pong from one end of a string of lights to
+		# the other.
+		#
 		self.sync += [
 			counter.eq(counter + 1),
-			If(counter == int((sys_clk_freq/blink_freq)/2 -1),
+			If(counter == ticks,
 				counter.eq(0),
 				#
 				# This is the chaser code, it combines remembering
@@ -111,18 +175,35 @@ class Chaser(Module):
 			)
 		]
 		#
-		# This is the combinarial code. All it does is insure that
-		# the LEDs mirror the state of the display[0..16] signal.
+		# This is the combinatorial code. 
+		# This code sends the state of the display register to
+		# all 16 LEDs.
+		# 
 		#
 		self.comb += [
 			( 
-#			This doesn't work
-#
+			#
+			# While writing this code I learned of several things that
+			# didn't work in Migen/FHDL. I've left them in so that you
+			# can see what my guesses looked like.
+			#
+			# This doesn't work, it was my first attempt. Remember how I
+			# named all of those subsignals so that I could refer to
+			# individual LEDs? Given that definition there is no way to
+			# assign to all of them.
+			#
+			# Now you could define the entire PMOD port as an 8 bit signal.
+			# That would work and you could refer to individual pins with
+			# the : syntax. However I wanted to have 'named' leds.
+			#
 #			leds.eq(display[0:8]),
 #			more_leds.eq(display[8:16])
-#
-#			This works but is ugly
-#
+			#
+			# This does work but it is ugly and a lot of typing!
+			#
+			# Each LED is individually addressed and it pulls from one
+			# bit of the display register
+			#
 #			leds.led0.eq(display[0]),
 #			leds.led1.eq(display[1]),
 #			leds.led2.eq(display[2]),
@@ -139,22 +220,23 @@ class Chaser(Module):
 #			more_leds.led5.eq(display[13]),
 #			more_leds.led6.eq(display[14]),
 #			more_leds.led7.eq(display[15]),
-#
-#			This works and is easier to read. 
-#
+			#
+			# This works AND it is easier to read.
+			#
+			# By building that concatenation, you can assign to
+			# it all at once, and so that is what this does.
+			#
 			all_leds.eq(display)
 			)
 		]
 
 #
-# now we are going to instantiate our LED chaser running
-# at 10Hz.
+# now we instantiate our LED chaser.
 #
-led_module = Chaser(25, 12e6);
+led_module = Cylon(25);
 
 #
 # And "build" this into a bit file
 #
-
-platform.build(led_module)
+icebitsy.build(led_module)
 
