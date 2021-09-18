8ba2bb2b91d77d6a1225e07ac911a12112cb609d
Chuck McManis <cmcmanis@mcmanis.com>
2021-09-17 23:30:32 -0700

    Actually include the changed files

--- 03_display/display.py
+++ 03_display/display.py
@@ -6,6 +6,12 @@
 from migen import *
 from litex.build.generic_platform import *
 from litex_boards.platforms.icebreaker_bitsy import Platform
+#
+# This cleans up the code significantly. Here we have abstracted the
+# seven segment LED display PMOD into its own module. When we instantiate
+# it in our design we will wire it to signals in the design and tell
+# the module what PMOD port it is connected to.
+#
 from led7segment import SevenSegmentLedDisplay
 
 # we'll call it a bitsy of type Platform()
@@ -13,49 +19,64 @@ bitsy = Platform()
 
 
 class Counter(Module):
-	def __init__(self, blink_freq):
+	"""
+		This is a binary coded decimal counter module. When it counts up
+		at 'count_speed' counts per second it goes from 00 to 99 and rolls
+		over.
+	"""
+	def __init__(self, count_speed):
 		#
 		# This is going to be our count
 		#
 		count = Signal(8)
-		self.submodules += [SevenSegmentLedDisplay(bitsy, "PMOD1", 
-															value=count)]
-
-		print(f"Counter at {blink_freq} Hz\n")
-		############
-		# some debug code
-		#
-		bitsy.add_extension([("heartbeat", 0, Pins("PMOD3:7"))])
-		bitsy.add_extension([("heartbeat", 0, Pins("PMOD3:6"))])
-		hb = bitsy.request("heartbeat")
-		hb2 = bitsy.request("heartbeat")
+		gled = bitsy.request("user_ledg_n")
 
 		#
 		# The 'standard' divide by n clock divider
 		#
-		counter = Signal(24)
-		ticks = int((500e6/(blink_freq * bitsy.default_clk_period))) - 1
+		divisor = Signal(24)
+		ticks = int((500e6/(count_speed * bitsy.default_clk_period))) - 1
 
+		#
+		# The sequential logic increments the counter in 'count' at a rate
+		# determined by 'count_speed'. (it is in units of counts per second)
+		# Something to notice here is that the count is an 8 bit binary value
+		# but the logic "jumps" it by 7 when it hits 9. In this way 0x09 -> 0x10
+		# and any time the last four bits are 0x9 it jumps. This makes it
+		# a binary coded decimal counter and the display shows 00 -> 99 before
+		# rolling over again.
+		#
 		self.sync += [
-			hb.eq(~hb),	# this should clock at the 1/2 SYSCLK
-			counter.eq(counter + 1),
-			If(counter == ticks,
+			divisor.eq(divisor + 1),
+			If(divisor == ticks,
+				divisor.eq(0),
+				gled.eq(~gled),
 				If(count == 0x99,
 					count.eq(0)
 				).Elif(count[:4] == 9,
-					count.eq(count + 7)
+					count.eq(count + 0x7)
 				).Else(
 					count.eq(count + 1)
 				)
 			),
 		]
-		self.comb += [ hb2.eq(~hb) ]
-
+		self.comb += [ ]
+		#
+		# This bit instantiates the SevenSegmentLedDisplay module and
+		# "connects" the count signal to the signal 'value' in the
+		# module.
+		#
+		# Migen takes the datastructure of modules and elaborates them
+		# into a single module and then optimizes by eliminating dead
+		# branches and redundant code, Etc.
+		#
+		self.submodules += [SevenSegmentLedDisplay(bitsy, "PMOD1", 
+															value=count)]
 #
 # Now instantiate a counter, which instantiates an LED display
 # sub-module which is showing the count.
 #
-count_module = Counter(10);
+count_module = Counter(5);
 
 #
 # And "build" this into a bit file
